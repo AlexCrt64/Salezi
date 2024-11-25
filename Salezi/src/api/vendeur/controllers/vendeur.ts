ded3963e287e37741640f9d137a95a5360edd5f2
AlexCrt64 <alexandre.courtet@epitech.digital>
2024-11-25 14:53:53 +0100

    new

---
+++ Salezi/src/api/vendeur/controllers/vendeur.ts
@@ -0,0 +1,7 @@
+/**
+ * vendeur controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::vendeur.vendeur');
---
+++ Salezi/src/api/vendeur/services/vendeur.ts
@@ -0,0 +1,7 @@
+/**
+ * vendeur service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::vendeur.vendeur');
